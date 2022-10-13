08ce917a99d7ca6b1d7897f52babcd6eb07f2f0d
Rob Hoch <rwhochstadt@gmail.com>
2022-10-12 22:43:25 -0400

    Add files via upload

---
+++ JavaRefresher.js
@@ -0,0 +1,21 @@
+let firstName = "Robert";
+let lastName = "Hochstadt";
+let fullName = (`${firstName} ${lastName}`);
+//let fullName = firstName + " " + lastName;
+
+
+
+let popDet = 639111;
+
+let tripDet = (popDet * 3);
+
+
+let JavScriptIscool = true;
+
+let colors = ["red","orang","yellow","green","blue","indigo","violet"]
+
+
+
+console.log(fullName);
+console.log(popDet);
+console.log(tripDet);
