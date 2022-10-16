0d040bca5c52a29833f7543cfc291baf9f247c44
Rob Hoch <rwhochstadt@gmail.com>
2022-10-16 15:10:02 -0400

    Add files via upload

---
+++ heatingcooling.js
@@ -0,0 +1,24 @@
+// // let actualTemp = 64;
+
+// // let desiredTemp = 69;
+// let actualTemp = 76
+//     let desiredTemp = 72
+// if (actualTemp > desiredTemp) {
+//     console.log("Run A/C")
+// } else if  (actualTemp < desiredTemp){
+//     console.log("Run Heat")
+// }else {
+//     console.log("Standby!")
+// }
+
+let tempCelcius = 10;
+let targetUnit = "F";
+// K = tempCelcius + 273.15;
+// F = (tempCelcius * 1.8)=32;
+if (targetUnit = "C") {
+    console.log(tempCelcius + " degrees Celcius");
+} else if ((targetUnit = "F")) {
+    console.log(tempCelcius * 1.8 + 32 + " degrees Fahrenheit");
+} else {
+    console.log(tempCelcius + 273.15 + " degrees Kelvin");
+}
---
+++ longestName.js
@@ -0,0 +1,17 @@
+let name1 = "Bartholomew";
+
+let name2 = "Aristotle";
+
+let name3 = "Archibald";
+
+let name1Length = name1.length;
+let name2Length = name2.length;
+let name3Length = name3.length;
+
+
+
+if (name1Length > name2Length) {
+    console.log(`${name1}  has the longest name.`)
+} else if (name2Length > name1Length) {
+    console.log(`${name2}  has the longest name.`)
+}else if(name)
