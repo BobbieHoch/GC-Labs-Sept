2513c0cfbcd4118e4f82e868601163a0264a5042
Rob Hoch <rwhochstadt@gmail.com>
2022-10-16 15:26:14 -0400

    Add files via upload

--- longestName.js
+++ longestName.js
@@ -1,8 +1,8 @@
-let name1 = "Bartholomew";
+let name1 = "Bart Jones";
 
-let name2 = "Aristotle";
+let name2 = "Aristotle Onassis";
 
-let name3 = "Archibald";
+let name3 = "Archibald Smith";
 
 let name1Length = name1.length;
 let name2Length = name2.length;
@@ -10,8 +10,18 @@ let name3Length = name3.length;
 
 
 
-if (name1Length > name2Length) {
+if (name1Length > name2Length && name1Length > name3Length) {
     console.log(`${name1}  has the longest name.`)
-} else if (name2Length > name1Length) {
+} else if (name2Length > name1Length && name2Length > name3Length) {
     console.log(`${name2}  has the longest name.`)
-}else if(name)
+} else if (name3Length > name2Length && name3Length > name1Length) {
+    console.log(`${name3}  has the longest name.`)
+} else if (name1Length === name2Length && name3Length !== name1Length) {
+    console.log(`${name1} and ${name2} tie for the longest name.`);
+} else if (name2Length === name3Length && name1Length < name2Length) {
+    console.log(`${name2} and ${name3} tie for the longest name`);
+} else if (name1Length === name3Length && name2Length < name1Length) {
+    console.log(`${name1} and ${name3} tie for the longest name.`);
+} else {
+    console.log(`All three names are the same length.`);
+}
